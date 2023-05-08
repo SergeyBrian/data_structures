6dfe8532d5dd6f3daa3cc161d98611edf3583b7f
Sergey Brian <s.brianski@yahoo.com>
2023-05-09 00:30:19 +0300

    Remove temporary code from main.c

--- 2_database/main.c
+++ 2_database/main.c
@@ -23,36 +23,21 @@ void parse_params(char **params, int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
-    char params[MAX_PARAM_SIZE][MAX_PARAMS_COUNT];
+    char *params[MAX_PARAMS_COUNT];
     parse_params(params, argc, argv);
 
     List *DB = db_init();
 
+    FILE *input_file = fopen(params[PARAM_INPUT_FILE_NAME], "r");
 
-    db_insert(DB,
-              "first_name=hello,last_name=Ivan,phone=79998887766,middle_name=ok,money=4.4,min_money=18,status=normal");
-    db_insert(DB,
-              "first_name=huy,last_name=Ivan,phone=79998887766,middle_name=ok,money=10.4,min_money=18,status=normal");
-    db_insert(DB,
-              "first_name=hello1,last_name=fucker,phone=79998887766,middle_name=ok,money=4.4,min_money=18,status=only_incoming_calls");
-    db_insert(DB,
-              "first_name=hello2,last_name=fucker,phone=79998887766,middle_name=ok,money=4.4,min_money=18,status=disabled");
-    db_insert(DB,
-              "first_name=hello3,last_name=fucker,phone=79998887766,middle_name=ok,money=4.4,min_money=18,status=normal");
-
-    List *recs;
-
-//    recs = db_select(DB, 3, "last_name=='Ivan',money>100,status/in/['normal', 'only_incoming_calls'],min_money<50");
-//    recs = db_select(DB, 1, "last_name=='Ivan',money==4.4,phone==79998887766");
-    recs = db_select(DB, 0, "status/in/['disabled', 'only_incoming_calls']");
-
-    list_iter(recs) {
-        DBRecord *rec = cast(DBRecord *, it->value);
-        printf("%s\n", rec->first_name);
+    char command[MAX_INPUT] = "";
+    while (fgets(command, MAX_INPUT, input_file)) {
+        printf("%s", command);
     }
 
-    list_destroy(recs);
     db_destroy(DB);
+
+
     printf("\n======================\n");
     printf("malloc: %d\ncalloc: %d\nfree: %d\nmalloc + calloc - free: %d", malloc_count, calloc_count, free_count,
            malloc_count + calloc_count - free_count);
